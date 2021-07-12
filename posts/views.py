a78fe40b975d35d41d82524136873c1d8c5c7757
Dmitriy_Nazymok <dnazymok@gmail.com>
2021-07-12 20:06:10 +0300

    form

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Post
+from django import forms
 
 
 def index(request):
@@ -20,7 +21,12 @@ def add(request):
             "posts/add.html",
         )
     elif request.method == "POST":
-        post_title = request.POST["title"]
-        post_content = request.POST["content"]
-        Post(title=post_title, content=post_content).save()
-        return redirect("/posts")
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post(title=form.cleaned_data["title"], content=form.cleaned_data["content"]).save()
+            return redirect("/posts")
+        return render(request, "posts/add.html", context={"form": form})
+
+class PostForm(forms.Form):
+    title = forms.CharField(min_length=5)
+    content = forms.CharField(max_length=100)
