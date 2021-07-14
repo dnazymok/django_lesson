f618638400a889758b20e4d8bb8e818c84da33e7
Dmitriy_Nazymok <dnazymok@gmail.com>
2021-07-14 20:15:32 +0300

    add m2m admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Category
 
-admin.site.register(Post)
+
+class PostCategoriesInline(admin.TabularInline):
+    model = Post.categories.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on')
+    inlines = [PostCategoriesInline]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 
